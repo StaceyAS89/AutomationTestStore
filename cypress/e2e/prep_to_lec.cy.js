c3e2c976cbfd00733afcef186b25dd359665e47b
“Anastasiia <anastasiiaforapple@gmail.com>
2023-02-16 14:05:07 +0100

    lecture faker json

--- cypress/e2e/prep_to_lec.cy.js
+++ cypress/e2e/prep_to_lec.cy.js
@@ -1,50 +1,52 @@
 ///<reference types="cypress"/>
+
+import { faker } from '@faker-js/faker';
+import * as user from '../fixtures/user.json';
+
+user.firstName= faker.name.firstName();
+user.lastName = faker.name.lastName();
+user.email = faker.internet.email();
+user.address = faker.address.streetAddress();
+user.city = faker.address.city();
+user.postCode = faker.address.zipCode('####');
+user.userName = faker.internet.userName();
+user.password = faker.internet.password(15);
+
 beforeEach('', () => {
+  cy.log('Open website home page')
   cy.visit('https://automationteststore.com/')
+
+  cy.log('Open login page')
   cy.get('.block_2 > #customernav')
   .click()
   .should('have.text', '\n\t\n\t\tLogin or register\n\t\n');
 })
-
   it('Registration', () => {
-    cy.get('.pull-right[title="Continue"]')
-    .click();
-
-    cy.get('#AccountFrm_firstname')
-    .type('Anastasiia')
-    .should('have.value', 'Anastasiia');
-
-    cy.get('#AccountFrm_lastname')
-    .type('Lazarenko')
-    .should('have.value', 'Lazarenko');
-
-    cy.get('#AccountFrm_email')
-    .type('anastasia@gmail.com')
-    .should('have.value', 'anastasia@gmail.com');
-
-    cy.get('#AccountFrm_telephone')
-    .type('+380934216460')
+    cy.log('Open website registartion page')
+    cy.get('.pull-right[title="Continue"]').click();
+    cy.get('#AccountFrm_firstname').type(user.firstName)
+    .should('have.value', user.firstName);
+    cy.get('#AccountFrm_lastname').type(user.lastName)
+    .should('have.value', user.lastName);
+    cy.get('#AccountFrm_email').type(user.email)
+    .should('have.value', user.email);
+    cy.get('#AccountFrm_telephone').type('+380934216460')
     .should('have.value', '+380934216460');
-
     cy.get('#AccountFrm_fax')
     .type('+380442369253')
     .should('have.value', '+380442369253');
-
     cy.get('#AccountFrm_company')
     .type('ITCompany')
     .should('have.value', 'ITCompany');
-
     cy.get('#AccountFrm_address_1')
-    .type('Address 1')
-    .should('have.value', 'Address 1');
-
+    .type(user.address)
+    .should('have.value', user.address);
     cy.get('#AccountFrm_address_2')
     .type('Address 2')
     .should('have.value', 'Address 2');
-
     cy.get('#AccountFrm_city')
-    .type('Kyiv')
-    .should('have.value', 'Kyiv');
+    .type(user.city)
+    .should('have.value', user.city);
 
     cy.get('#AccountFrm_zone_id')
     .select('Glasgow')
@@ -53,8 +55,8 @@ beforeEach('', () => {
     .should('have.text', 'Glasgow');
 
     cy.get('#AccountFrm_postcode')
-    .type('03-287')
-    .should('have.value', '03-287');
+    .type(user.postCode)
+    .should('have.value', user.postCode);
 
     cy.get('select#AccountFrm_country_id')
     .select('United Kingdom')
@@ -62,56 +64,53 @@ beforeEach('', () => {
     .children('[value="222"]')
     .should('have.text', 'United Kingdom');
 
-
     cy.get('#AccountFrm_loginname')
-    .type('StaceyASSSSSSSSSSSSS')
-    .should('have.value', 'StaceyASSSSSSSSSSSSS');
+    .type(user.userName)
+    .should('have.value', user.userName);
 
     cy.get('#AccountFrm_password')
-    .type('12345')
-    .should('have.value', '12345')
+    .type(user.password)
+    .should('have.value', user.password)
     .and('have.attr', 'type')
     .and('include', 'password');
   
-
     cy.get('#AccountFrm_confirm')
-    .type('12345')
-    .should('have.value', '12345')
+    .type(user.password)
+    .should('have.value', user.password)
     .and('have.attr', 'type')
-    .and('include', 'password');
+    .and('include', 'password')
 
+  cy.log('Check checkboxes')
     cy.get('#AccountFrm_newsletter1')
     .check()
     .should('include.checked', 'true');
-
     cy.get('#AccountFrm_agree')
     .check()
     .should('include.checked', 'true');
-
     cy.get('.btn-orange.pull-right')
     .click();
 
+    cy.log('Verify user first name on account page')
     cy.get('span.maintext')
     .should('have.text', ' Your Account Has Been Created!');
 
-    cy.get('.side_account_list').contains('Logoff')
-    .click();
+    console.log(user)
   })
 
   it('Login', () => {
+    cy.log('Check user is unathorized')
+    cy.getCookie('customer').should('be.null')
+    cy.log('Athorize user')
     cy.get('#loginFrm_loginname')
-    .type('StaceyASSSSSSSSSSS')
-    .should('have.value', 'StaceyASSSSSSSSSSS');
+    .type(user.userName)
+    .should('have.value', user.userName);
 
     cy.get('#loginFrm_password')
-    .type('12345')
-    .should('have.value', '12345');
-
-    cy.get('[title="Login"]')
+    .type(user.password)
+    .should('have.value', user.password);
+   cy.get('[title="Login"]')
     .click();
+    cy.get('h1 span.subtext').should('have.text', user.firstName)
   })
 
-  after('', () => {
-    cy.get('.side_account_list').contains('Logoff')
-    .click();
-  })
+
