32e34fb8cdca8d07b1156c2f1312dc8255040ff1
“Anastasiia <anastasiiaforapple@gmail.com>
2023-03-24 13:59:09 +0100

    cypress.config is updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
   viewportHeight: 1080,
